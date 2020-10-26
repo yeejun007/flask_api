619ec7bcfc80b03013e0994dea83b7e44dec7562
yeejun007 <dlwns007@gmail.com>
2020-10-26 11:07:43 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+app.tweets = []
+
+
+@app.route("/", methods=['GET'])
+def main():
+    return "Hello!"
+
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+
+    return jsonify(new_user)
+
+
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+
+    if len(tweet) > 300:
+        return '300자를 초과했습니다', 400
+
+    user_id = int(payload['id'])
+
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return '', 200
+
+
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return '사용자가 존재하지 않습니다', 400
